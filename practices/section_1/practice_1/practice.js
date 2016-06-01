b11eccf11d45cc6dee8c42045f52de8b3f2afd81
xiongchaohu <1732139715@qq.com>
2016-06-01 16:17:13 +0800

    'add'

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,17 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  	var temp=[];
+	for(var i=0;i<collection_a.length;i++)
+	{
+
+		if(collection_b.indexOf(collection_a[i])!=-1)
+		{
+			temp.push(collection_a[i]);
+		}
+			
+	}
+	return temp;
+		
 }
 
 module.exports = collect_same_elements;
